ab90dfca0c382a5f834da16821ead0c188f316d1
sean8purdue <sean8purdue@gmail.com>
2015-12-20 17:30:27 -0500

    ex9_1 print number arrays

--- ex9/ex9.c
+++ ex9/ex9.c
@@ -3,7 +3,12 @@
 int main(int argc, char *argv[])
 {
 	int numbers[4] = {0};
-	char name[4] = {'a'};
+	//char name[4] = {'a'};
 
 	//first, print them out raw
+	printf("numbers: %d %d %d %d\n",
+			numbers[0], numbers[1],
+			numbers[2], numbers[3]);
+
+	return 0;
 }
