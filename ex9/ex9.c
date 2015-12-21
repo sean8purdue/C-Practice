3f15b8f3efa13e458845214579910494b8067472
sean8purdue <sean8purdue@gmail.com>
2015-12-21 12:23:24 -0500

    1. test uninitialized number and character arrays, output is random,in file init_number1; 2. in file ex9.c: init every element of arrays mannully

--- ex9/ex9.c
+++ ex9/ex9.c
@@ -15,5 +15,29 @@ int main(int argc, char *argv[])
 	printf("name: %c %c %c %c\n",
 			name[0], name[1],
 			name[2], name[3]);
+
+	printf("name: %s\n", name);
+
+	// setup the numbers
+	numbers[0] = 1;
+	numbers[1] = 2;
+	numbers[2] = 3;
+	numbers[3] = 4;
+
+	// setup the name
+	name[0] = 'Z';
+	name[0] = 'e';
+	name[0] = 'd';
+	name[0] = '\0';
+
+	// print them out initially
+	printf("numbers: %d %d %d %d\n",
+			numbers[0], numbers[1],
+			numbers[2], numbers[3]);
+
+	printf("name: %c %c %c %c\n",
+			name[0], name[1],
+			name[2], name[3]);
+
 	return 0;
 }
--- ex9/init_number1.c
+++ ex9/init_number1.c
@@ -6,12 +6,38 @@ int main(int argc, char *argv[])
 	 */
 	int numbers[4] = {1};
 	char name[4] = {'a'};
+	// test non initialized number and character array
+	int numbers1[3];
+	char name1[3];
+
+	// print non initialized number out
+	// result: numbers1: 1 0 4196077  random numbers
+	printf("numbers1: %d %d %d\n",
+			numbers1[0], numbers1[1],
+			numbers1[2]);
 
 	// first, print them out raw
 	printf("numbers: %d %d %d %d\n",
 			numbers[0], numbers[1],
 			numbers[2], numbers[3]);
 
+	// result: random numbers
+	printf("name1: %d %d %d\n",
+			name1[0], name1[1],
+			name1[2]);
+	// result: non-sense charater
+	printf("name1: %c %c %c\n",
+			name1[0], name1[1],
+			name1[2]);
+	/*
+	   sean@seanlinux:~/git/sec/ex9$ ./init_number1 
+numbers1: 1 0 4196157
+numbers: 1 0 0 0
+name1: -56 -63 118
+name1: � � v
+name: 97 0 0 0
+name: a
+	 */
 //<<<<<<< HEAD
 	// To compare_print integer type of character array
 	printf("name: %d %d %d %d\n",
