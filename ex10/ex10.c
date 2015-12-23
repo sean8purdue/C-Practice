50d2284d514cb4308adef7f2ac11bfaa1d7000ad
sean8purdue <sean8purdue@gmail.com>
2015-12-23 12:30:01 -0500

    print every arguments that command line passing

--- ex10/ex10.c
+++ ex10/ex10.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
-int main()
+int main(int argc, char *argv[])
 {
+	int i = 0;
+
+	// go through each string in argv
+	// why am I skipping argv[0]?
+	for (i = 1; i < argc; i++) {
+		printf("arg %d: %s\n", i, argv[i]);
+	}
+	
+	return 0;
+
 
 }
