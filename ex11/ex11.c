5907fea85c6d8935dda5fe81da4867577d098cce
sean8purdue <sean8purdue@gmail.com>
2015-12-25 16:12:43 -0500

    print own string with 'while loop'

--- ex11/ex11.c
+++ ex11/ex11.c
@@ -12,6 +12,25 @@ int main(int argc, char *argv[])
 		i++;
 	}
 
+	char *states[] = {
+		"California", "Oregon",
+		"Washington", "Texas"
+	};
+
+	int num_states = 4;
+	//int i = 0;
+	// error: redefinition of ‘i’
+	i = 1;
+
+	while(i < num_states) {
+		printf("state %d: %s\n", i, states[i]);
+		i++;
+
+		// if forget i++, then will have unlimited loop,
+		// since i will always be 1, 
+	
+	}
+
 	return 0;	
 
 }
