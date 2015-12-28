cb2f294271be5efecf7a2ca74e5d396468781c91
sean8purdue <sean8purdue@gmail.com>
2015-12-28 13:31:27 -0500

    1.make the argv array overflow by adding 1 to argc

--- ex14/ex14.c
+++ ex14/ex14.c
@@ -36,6 +36,18 @@ int can_print_it(char ch)
 
 int main(int argc, char *argv[])
 {
+	//print_arguments(argc+1, argv);
+	/* when call print_arguments inside main,
+	   adding 1 to argc, so that it goes past
+	   the end of argv arrya
+
+Result: Segmentation fault
+sean@seanlinux:~/git/sec/ex14$ ./ex14 arg
+'e' == 101 'x' == 120 
+'a' == 97 'r' == 114 'g' == 103 
+Segmentation fault (core dumped)
+sean@seanlinux:~/git/sec/ex14$ 
+	 */
 	print_arguments(argc, argv);
 	return 0;
 }
