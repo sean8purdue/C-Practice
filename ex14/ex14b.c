88a5c5e228b55ef3b8afc17d7a982fe660b66155
sean8purdue <sean8purdue@gmail.com>
2015-12-28 13:14:50 -0500

    delete forward

---
+++ ex14/ex14b.c
@@ -0,0 +1,50 @@
+/* Using Functions
+ */
+#include <stdio.h>
+#include <ctype.h>
+
+// forward declarations
+//int can_print_it(char ch);
+//void print_letters(char arg[]);
+/* comment forward declaration, make the compiler complains about
+   cann't find these two functions
+   
+result: 
+1 error:
+    can't compile: conflicting types for 'print_letters'
+2. waring:
+implicit declaration of function 'can_print_it' 
+ */
+        
+void print_arguments(int argc, char *argv[])
+{
+	int i = 0;
+    
+	for(i = 0; i < argc; i++) {
+		print_letters(argv[i]);
+	}
+}
+
+void print_letters(char arg[])
+{
+    int i = 0;
+    for(i = 0; arg[i] != '\0'; i++) {
+    	char ch = arg[i];
+	
+	if(can_print_it(ch)) {
+		printf("'%c' == %d ", ch, ch);
+	}
+    }
+    printf("\n");
+}
+
+int can_print_it(char ch)
+{
+	return isalpha(ch) || isblank(ch);
+}
+
+int main(int argc, char *argv[])
+{
+	print_arguments(argc, argv);
+	return 0;
+}
