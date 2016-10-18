acb35e80851603c77da7ca99e6fbf4302a93341d
Sean <sean8purdue@gmail.com>
2016-10-17 23:24:31 -0400

    cpp, expand header file Test

---
+++ arrayT_1017_16/cppT/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("hello\n");
+}
+
+// run cpp hello.c | less
+// to see expand header <stdio.h>
+
---
+++ arrayT_1017_16/cppT/myHeader.c
@@ -0,0 +1,15 @@
+/*#include "header.h"*/
+#include <stdio.h>
+
+int add(int, int);
+
+int main()
+{
+	int i = 1;
+	int j = 2;
+
+	int k;
+
+	k = add(i, j);
+	printf("%d\n", k);
+}
