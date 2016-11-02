78c314af30f76ecb6db4961a78dba6f287372abd
Sean <sean8purdue@gmail.com>
2016-11-02 19:19:15 -0400

    Set global variable g1 for print2()

--- scope_1102_16/globalVariable.c
+++ scope_1102_16/globalVariable.c
@@ -4,7 +4,16 @@ void print1(char *p1) {
 	printf("%s\n", p1);
 }
 
+int g1 = 1;
+
+void print2(char *p2) {
+	printf("%s\n", p2);
+	printf("print2 print g1: %d\n", g1);
+}
+
 int main() {
+	printf("main print g1: %d\n", g1);
 	printf("Hello\n");
-	print1("ss");
+	print1("print1");
+	print2("print2");
 }
