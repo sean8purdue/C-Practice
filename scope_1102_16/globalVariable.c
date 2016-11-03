65b33502da0cec9ec5f7de8febe452b88802f046
Sean <sean8purdue@gmail.com>
2016-11-02 22:41:02 -0400

    global string test2

--- scope_1102_16/globalVariable.c
+++ scope_1102_16/globalVariable.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
+char * str2 = "global string for all\n";
+
 void print1(char *p1) {
 	printf("%s\n", p1);
 	//can't access globale variable g1 which declared after
 	/*printf("print2 print g1: %d\n", g1);*/
+	printf("Print1 print str1: %s\n", str2);
+	//
+	//can't access globale variable str1 which declared after
+	/*printf("Print2 print str1: %s\n", str1);*/
 }
 
 int g1 = 1;
@@ -18,8 +24,12 @@ void print2(char *p2) {
 int main() {
 	printf("main print g1: %d\n", g1);
 	printf("main print str1: %s\n", str1);
+	printf("main print str2: %s\n", str2);
+
 	str1 = "main change global str1";
-	printf("Hello\n");
+	str2 = "main change str2\n";
+
+	printf("call function print1 & print2\n");
 	print1("print1");
 	print2("print2");
 }
