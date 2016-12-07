99aa6042e6b517b27a1a0ce72c12b733966083f8
Sean <sean8purdue@gmail.com>
2016-12-06 21:40:50 -0500

    change const char pointer

--- charPointer_1013_16/constChar.c
+++ charPointer_1013_16/constChar.c
@@ -10,8 +10,8 @@ int main() {
 	// const char* means that the contents of the string are constant. You can have different pointers referring to the same constant memory. 
 	// In other words, the pointers are different variables with the same value. Remember that the value of a pointer is the address of the memory to which it refers.
 	
-	/*// change s1, let s1 point to s2*/
-	/*s1 = s2;*/
-	/*printf("After change s1: Const String1 is \"%s\".\n", s1);*/
+	// change s1, let s1 point to s2
+	s1 = s2;
+	printf("After change s1: Const String1 is \"%s\".\n", s1);
 
 }
