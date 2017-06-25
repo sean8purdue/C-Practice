ea4981cb66296ad19b2254e3e257a888fd18a4cf
Sean <sean8purdue@gmail.com>
2017-06-25 17:30:43 -0400

    pass array by address and modify original array in callee

--- charPointer_1013_16/PassingArray.md
+++ charPointer_1013_16/PassingArray.md
@@ -44,3 +44,51 @@ in arrayPass mark[2] = 0x7fff54ce7a24
 ~~~
 
 We can see the array always pass by address.
+
+## Test in vyValue.c
+
+~~~c
+void byval_func(int i[]) {
+    int y;
+    /* print the content: of the integer array. */
+    for (y=0; y<3;  y++) 
+        printf("%d\n", i[y]);
+
+    printf("in byval i = %p\n", i);
+    printf("in byval &i = %p\n", &i);
+    printf("in byval &i[0] = %p\n", &i);
+    printf("in byval &i[1] = %p\n", &i[1]);
+    printf("in byval &i[2] = %p\n", &i[2]);
+    
+    // change original array contents
+    i[0] = 21;
+    i[1] = 22;
+}
+~~~
+
+Result: We notice in 
+
+`byval i = 0x7fff527a49a0` = `in main &x = 0x7fff527a49a0`
+`in byval &i[1] = 0x7fff527a49a4` =  `main &x[1] = 0x7fff527a49a4`
+`byval &i[2] = 0x7fff527a49a8 ` =  `main &x[2] = 0x7fff527a49a8`
+
+They have the same address.
+
+~~~
+in main &x = 0x7fff527a49a0
+in main &x[0] = 0x7fff527a49a0
+in main &x[1] = 0x7fff527a49a4
+in main &x[2] = 0x7fff527a49a8
+0
+1
+2
+in byval i = 0x7fff527a49a0
+in byval &i = 0x7fff527a4948
+in byval &i[0] = 0x7fff527a4948
+in byval &i[1] = 0x7fff527a49a4
+in byval &i[2] = 0x7fff527a49a8
+in main x[0] = 21
+in main x[1] = 22
+~~~
+
+And **Change value in callee will change caller's array**.
--- charPointer_1013_16/byValue.c
+++ charPointer_1013_16/byValue.c
@@ -1,7 +1,7 @@
-/*#include <atdio.h>*/
 #include <stdio.h>
 
 void byval_func(int[]);
+void byval_char(char[]);
 
 int main () {
     int x[10];
@@ -10,12 +10,27 @@ int main () {
     for (y=0; y<3;   y++)
         x[y] = y;
 
-    printf("in main &x[0] = %p\n", &x);
+    printf("in main &x = %p\n", &x);
+    printf("in main &x[0] = %p\n", &x[0]);
     printf("in main &x[1] = %p\n", &x[1]);
     printf("in main &x[2] = %p\n", &x[2]);
 
     /* Call byval_func() ,passing the x array by value. */
     byval_func(x);
+    
+    // change array contents in arrayPass()
+    printf("in main x = %d\n", x[0]);
+    printf("in main x[1] = %d\n", x[1]);
+    printf("\n");
+
+    // char array test
+    char c[3] = {'a', 'b', 'c'};
+    printf("in main &c = %p\n", &c);
+    printf("in main &c[0] = %p\n", &c[0]);
+    printf("in main &c[1] = %p\n", &c[1]);
+    printf("in main &c[2] = %p\n", &c[2]);
+
+    byval_char(c);
 }
 
 /* The byval_function receives an integer array by value. */
@@ -26,7 +41,24 @@ void byval_func(int i[]) {
         printf("%d\n", i[y]);
 
     printf("in byval i = %p\n", i);
+    printf("in byval &i = %p\n", &i);
     printf("in byval &i[0] = %p\n", &i);
     printf("in byval &i[1] = %p\n", &i[1]);
     printf("in byval &i[2] = %p\n", &i[2]);
+    
+    // change original array contents
+    i[0] = 21;
+    i[1] = 22;
+}
+
+void byval_char(char ch[]) {
+    for (int i = 0; i < 3; i++) {
+        printf("in byval char[%d] = %c\n", i, ch[i]);
+    }
+
+    printf("in byval ch = %p\n", ch);
+    printf("in byval &ch = %p\n", &ch);
+    printf("in byval ch[0] = %c, &ch[0] = %p\n", ch[0], &ch);
+    printf("in byval ch[1] = %c, &ch[1] = %p\n", ch[1], &ch[1]);
+    printf("in byval ch[2] = %c, &ch[2] = %p\n", ch[2], &ch[2]);
 }
