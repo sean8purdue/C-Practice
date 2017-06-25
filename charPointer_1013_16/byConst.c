c4ec2a94bfad0eddf248059f197bf2af28624047
Sean <sean8purdue@gmail.com>
2017-06-25 18:01:12 -0400

    const array which can't be modified in callee

--- charPointer_1013_16/byConst.c
+++ charPointer_1013_16/byConst.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 
 void byConst(char * const);
+void constArray(char const *);
 
 int main() {
     char ch1[3] = {'a', 'b', 'c'};
     const char ch2[3] = {'g', 'h', 'k'};
 
+    // const pointer point to char array
     byConst(ch1);
     for (int i = 0; i < 3; i++) {
         printf("in byConst %c\n", *(ch1 + i));
     }
+
+    printf("\nConst Array Test\n");
+    // const array
+    constArray(ch2);
 
 }
 
@@ -17,5 +23,13 @@ void byConst(char * const ch) {
     for (int i = 0; i < 3; i++) {
         printf("in byConst %c\n", *(ch+i));
         *(ch+i) = 'Y';
+    }
+}
+
+void constArray(char const * ch) {
+    for (int i = 0; i < 3; i++) {
+        printf("in byConst %c\n", *(ch+i));
+
+        /**(ch+i) = 'Y'; // compile error when compile*/
     }
 }
